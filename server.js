04057c341b3e06f3b5a1e0d714b5dd8db96ed447
jhersbo <jhersbo@gmail.com>
2022-02-09 12:58:29 -0600

    client password must be a string

--- server.js
+++ server.js
@@ -1,3 +1,5 @@
+const { Sequelize } = require('sequelize')
+
 // DEPENDENCIES
 const express = require('express')
 const app = express()
@@ -6,6 +8,15 @@ const app = express()
 require('dotenv').config()
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
+// SEQUELIZE CONNECTION
+const sequelize = new Sequelize(process.env.PG_URI)
+
+try {
+    sequelize.authenticate() 
+    console.log(`Connected with Sequelize at ${process.env.PG_URI}`) 
+} catch(err) {
+    console.log(`Unable to connect to PG: ${err}`) 
+}
 
 // ROOT
 app.get('/', (req, res) => {
